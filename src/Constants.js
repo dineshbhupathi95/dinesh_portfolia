686850c8ba9df918eaa16341008980cc1183047b
Dinesh Bhupathi <dineshbhupathi@Dineshs-MacBook-Air.local>
2025-05-22 20:05:38 +0530

    first commit

---
+++ src/Constants.js
@@ -0,0 +1,38 @@
+import AUTOMATION from "./Assets/Projects/3automation.jpg";
+import COAST from "./Assets/Projects/coast.jpg";
+import DROPSHIP from "./Assets/Projects/dropship.jpg";
+import GOOD_FOOD_DISCOUNTS from "./Assets/Projects/goodFoodDiscounts.png";
+import GUAMEDIA from "./Assets/Projects/guamedia.jpg";
+import HSP from "./Assets/Projects/hsp.png";
+import COSMOS from "./Assets/Projects/cosmos.jpg";
+import TECHSPEC from "./Assets/Projects/techspec.png";
+import TRAVELWELL from "./Assets/Projects/travelwell.png";
+
+
+export const PROJECTS = [
+  {
+    image: COSMOS,
+    name: "Captures+",
+    technologyUsed: "Python,FastAPI,MongoDB,AWS",
+    description: "",
+    url: "",
+  },
+];
+
+export const SKILLS = [
+  { name: "Python", initialRating: 4 },
+  { name: "Django", initialRating: 4 },
+  { name: "Fastapi", initialRating: 4 },
+  { name: "MySQL", initialRating: 4 },
+  { name: "Reactjs", initialRating: 3 },
+  { name: "ElasticSearch", initialRating: 3 },
+  { name: "MongoDB", initialRating: 3 },
+  { name: "Html", initialRating: 4 },
+  { name: "CSS", initialRating: 4 },
+  { name: "RESTAPI", initialRating: 3 },
+  { name: "Graphql", initialRating: 4 },
+  { name: "AntD | ReactStrap | Material UI", initialRating: 4 },
+  { name: "Git", initialRating: 4 },
+];
+
+export const TOOLS = ["Visual Studio Code", "Git",  "Postman"]
---
+++ src/components/Home/Home.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/passportphoto.jpeg";
+import SocialMedia from "../SocialMedia";
+import TypeWriter from "./TypeWriter";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={6} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> DINESH BHUPATHI</strong>
+              </h1>
+
+              <p className="heading-description blockquote">
+              Dynamic Python Developer with 5+ years of software development experience,
+               specializing in creating robust and scalable web applications. Proficient in leveraging Pythons
+                versatility and efficiency to deliver innovative solutions that meet and exceed client expectations. 
+                With a keen eye for detail and a passion for problem solving, 
+                
+              </p>
+
+              <div className="heading-type">
+                <TypeWriter />
+              </div>
+            </Col>
+
+            <Col md={5}>
+              <img src={myImg} className="profile-pic" alt="avatar" />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container fluid className="home-about-section" id="about">
+        <Container>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>Get in Touch</h1>
+              <p>
+                {" "}
+                Whether you want to get in touch, or talk about a project
+                collaboration.
+                <br />
+                <strong>Feel free to connect with me</strong>
+              </p>
+              <SocialMedia />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
+}
+
+export default Home;
